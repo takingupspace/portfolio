7453bbb8c2d4675d462586ca8ea836911d0f8c4d
takingupspace <belllabenthusiast@gmail.com>
2022-01-13 10:17:56 -0600

    about-to-add-tailwind

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import styles from './About.module.css'
+import styles from './About.module.css';
 export default function Welcome(props){
     return(
-      <section className={`outer-about`}>
-          <div className={`about1`}>
-              <div className={`about2`}>
-                  <h1 className={`about-intro`}>Hi, I'm Travis</h1><br />
+      <section className={`${styles}`}>
+          <div >
+              <div >
+                  <h1 >Hi, I'm Travis</h1><br />
                   I love building web applications
-                  <p>I look forward to utilizing new technologies to build different web applications</p>
+                  <p className={`${styles.intro}`}>I look forward to utilizing new technologies to build different web applications</p>
               </div>
               <div className={`about-nav`}>
-                  <a href='#contact' className={`about-contact`}>Contact</a>
-                  <a href='#projects' className={`about-projects`}>Projects</a>
+                  <a href='#contact'>Contact</a>
+                  <a href='#projects'>Projects</a>
               </div>
           </div>
           <div className={`about-img`}>
-              <img className={`about-img1`} 
-                  alt="default react"
+              <img alt="default react"
                   src='./logo192.png'
               />
           </div>
