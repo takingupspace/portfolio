8d5206f5ffbe368387b4d6b58d8ce4b415b82d5b
takingupspace <belllabenthusiast@gmail.com>
2022-01-13 09:56:55 -0600

    about-skeleton-styling-next

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,3 +1,25 @@
+import React from 'react';
+import styles from './About.module.css'
 export default function Welcome(props){
-    return <h1>This is the about section {props.name}</h1>
+    return(
+      <section className={`outer-about`}>
+          <div className={`about1`}>
+              <div className={`about2`}>
+                  <h1 className={`about-intro`}>Hi, I'm Travis</h1><br />
+                  I love building web applications
+                  <p>I look forward to utilizing new technologies to build different web applications</p>
+              </div>
+              <div className={`about-nav`}>
+                  <a href='#contact' className={`about-contact`}>Contact</a>
+                  <a href='#projects' className={`about-projects`}>Projects</a>
+              </div>
+          </div>
+          <div className={`about-img`}>
+              <img className={`about-img1`} 
+                  alt="default react"
+                  src='./logo192.png'
+              />
+          </div>
+      </section>  
+    );
 }
