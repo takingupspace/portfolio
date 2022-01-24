c5aa735aaa689633309d74c872e37252a670ffd8
takingupspace <belllabenthusiast@gmail.com>
2022-01-24 11:55:33 -0600

    fixed-about-me-and-contact-me

--- client/src/content.js
+++ client/src/content.js
@@ -2,11 +2,11 @@ const content = [
     {
         id: 1,
         title: 'About Me',
-        body: "My interest in Web Development begun in 2017. I explored different courses on Udemy, where I first started off with HTML/CSS. That front-end styling evolved into  an interest in learning a programming language in JavaScript, and then Java. My " +
-        "interest in Java led me to want to pursue a deeper level of understanding in object-oriented design, which led to a desire to learn more about software development as fundamentally low a level as I was able to. I decided to" +
-        "enroll in the Computer Science program in that same year, which eventually led me to the University of Texas at San Antonio. My time at UTSA gave me the opportunity to follow my passions and fully explore the areas of the technological " + 
-        "domains that I wanted to pursue. I joined a robotics club, participated in a hackathon where we worked on a software development project, and was involved in several gorup projects with my fellow colleagues that contributed " +
-        "to advancing my domain knowledge and technical skill. In conclusion, I can't wait to be part of an innovative and progressive team that contributes to the advancement of some practical area of life.",
+        body: "My interest in Web Development began in 2017. I explored different courses on Udemy, initially picking up HTML/CSS. That knowledge gained from front-end styling evolved into  an interest in learning a programming language. First, I delved into JavaScript and that led to an exploration of Java. My " +
+        "interest in Java led me to pursue a deeper level of understanding of object-oriented design. This eventually guided my desire to learn toward the lowest level of fundamentals for software development. I decided to " +
+        "enroll in the Computer Science program in that same year, which eventually led me to the University of Texas at San Antonio. My time at UTSA gave me the opportunity to pursue my passions and fully explore the sub-domains under the technological umbrella " + 
+        "that most interested me. I joined a robotics club, participated in a hackathon where we worked on a software development project, and was involved in several gorup projects with my fellow colleagues that contributed " +
+        "to advancing my domain knowledge, as well as my technical and team-oriented skills.",
         image : 'NO IMAGE ATM',
     },
     {
@@ -18,7 +18,7 @@ const content = [
     {
     id: 3,
     title: 'Contact Me',
-    body: "[FILLER TEXT]",
+    body: "I'm excited to join an innovative and progressive company where I can grow my technical and team-oriented skills. My contact details (GitHub, LinkedIn, and Twitter) are listed below in the footer, under their appropriate icons.",
     image : 'NO IMAGE ATM',
     }
 ]
